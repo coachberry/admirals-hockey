1e1fb2779b68fc1ffacde4b3dceb8b575be9fb88
Matt Berry <coachberry03@gmail.com>
2026-06-01 22:44:30 -0500

    Fix page guard - hide nav links on all pages including homepage

--- assets/js/page-guard.js
+++ assets/js/page-guard.js
@@ -1,14 +1,18 @@
 (function() {
   var pageId = window.location.pathname.split('/').pop().replace('.html','');
-  if (!pageId || pageId === 'index') return;
+
   fetch('https://firestore.googleapis.com/v1/projects/admirals-hockey/databases/(default)/documents/settings/pages')
     .then(function(r) { return r.json(); })
     .then(function(data) {
       var fields = data.fields || {};
-      if (fields[pageId] && fields[pageId].booleanValue === false) {
+
+      // Redirect if this page is hidden (skip homepage)
+      if (pageId && pageId !== 'index' && fields[pageId] && fields[pageId].booleanValue === false) {
         window.location.href = '/index.html';
         return;
       }
+
+      // Hide nav links for all hidden pages on every page
       Object.keys(fields).forEach(function(id) {
         if (fields[id].booleanValue === false) {
           document.querySelectorAll('a[href*="/' + id + '.html"]').forEach(function(l) {
