0011a8045bce9ce3a7ec9ddfaa83232dbcf8fe87
Matt Berry <coachberry03@gmail.com>
2026-07-24 11:23:01 -0500

    Force service worker update by bumping version comment

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+// v2 - force update
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
